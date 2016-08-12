25d116f6ffcbc4d89a8c91a3af2acf5e581397df
Nipun Kumar <Nipun.Kumar@osscube.com>
2016-08-12 17:02:29 +0530

    this is test

--- ComplySight/src/framework/Browser.java
+++ ComplySight/src/framework/Browser.java
@@ -24,6 +24,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 		{
 			       Driver = null;
 System.out.println("NipunK");
+System.out.println("Kumar");
 		          if (Br.equalsIgnoreCase("FF"))
 
 		               {
