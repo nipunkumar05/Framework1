38b79cc76f29d5cee738f8bddc1c585b17cff4eb
Nipun Kumar <Nipun.Kumar@osscube.com>
2016-08-12 17:00:24 +0530

    dssds

--- ComplySight/src/framework/Browser.java
+++ ComplySight/src/framework/Browser.java
@@ -23,7 +23,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 		public static WebDriver browser(String Br)
 		{
 			       Driver = null;
-System.out.println("NK");
+System.out.println("NipunK");
 		          if (Br.equalsIgnoreCase("FF"))
 
 		               {
