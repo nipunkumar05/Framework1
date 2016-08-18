22854511c8a8ea403088fc5e2967ef8369f8e2e6
Nipun Kumar <Nipun.Kumar@osscube.com>
2016-08-18 13:33:13 +0530

    qqq

--- Git learning/src/p1/C2.java
+++ Git learning/src/p1/C2.java
@@ -4,7 +4,7 @@ public class C2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("C2 changed for 1st");
+		System.out.println("C2 changed for 1st2nd");
 
 	}
 
