8a371383c00d2c9ba62d570158f5a68826a465c0
nipunkumar05 <nipun.kumar@osscube.com>
2016-08-18 14:45:57 +0530

    Update c1.java

--- Git learning/src/p1/c1.java
+++ Git learning/src/p1/c1.java
@@ -6,7 +6,7 @@ public c1() {
 	int a;
 System.out.println("file updated 1_4_4");
 
-int b,c;
+System.out.println("file updated 1_4_5");
 System.out.println("file updated 1_4_6");
 
 	// TODO Auto-generated constructor stub
